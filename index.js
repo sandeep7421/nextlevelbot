7ffdc10f3e0f1dc1375f7aade07c38150089076f
sandeep7421 <sandeepdev7421@gmail.com>
2023-07-21 12:15:10 +0530

    modigy

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 // index.js
 require("./bin/kernel");
-let serverless = require("serverless-http");
 let express = require("express");
 let path = require("path");
 let logger = require("morgan");
-let cookieParser = require("cookie-parser");
-// let webRoutes = require("./routes/web");
 let apiRoutes = require("./routes/api");
 // Import the library:
 let cors = require("cors");
 let app = express();
 // view engine setup
-app.set("views", path.join(__dirname, "resources/views"));
-app.set("view engine", "ejs"); // either pug,twig etc
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 // Then use it before your routes are set up:
 
 app.use(cors());
-// app.use("/", webRoutes);
 app.use("/api/v1/", apiRoutes);
 
 // catch 404 and forward to error handler
@@ -46,4 +37,3 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
-module.exports.handler = serverless(app);
