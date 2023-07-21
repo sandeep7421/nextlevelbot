efd5c075dc57c7eb350f7f19ca188cb6908f5e64
sandeep7421 <sandeepdev7421@gmail.com>
2023-07-21 12:56:48 +0530

    rst

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ let Auth = middleware('Auth');
 let user = controller("Api/User/UserController");
 
 router.post("/register", [], (req, res) => {
-  return user.register(req, res);
+  return user.register(req, res); 
 });
 
 module.exports = router;
